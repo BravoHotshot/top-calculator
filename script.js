abc0c2bf7d6a4b6abb1708e74e1a7ede4bbfc602
Bravo Hotshot <98163699+BravoHotshot@users.noreply.github.com>
2024-07-08 09:54:58 +0530

    Switch button creation to JS to clean up HTML

--- script.js
+++ script.js
@@ -21,3 +21,63 @@ let operate = (a, op, b) => {
             break;
     }
 }
+
+function createButtons() {
+    let numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const buttons = document.querySelector(".buttons");
+
+    for (let rowNumber = 1; rowNumber < 5; rowNumber++) {
+        const row = document.createElement("div");
+        row.setAttribute("class", "buttonRow");
+
+        for (let buttonNumber = 1; buttonNumber < 5; buttonNumber++) {
+            const button = document.createElement("div");
+            button.setAttribute("class", "button")
+
+            if (rowNumber < 4) {
+                if (buttonNumber == 4) {
+                    switch (rowNumber) {
+                        case 1:
+                            button.textContent = "+";
+                            break;
+
+                        case 2:
+                            button.textContent = "-";
+                            break;
+
+                        case 3:
+                            button.textContent = "*";
+                            break;
+                    }
+                }
+
+                else {
+                    button.textContent = numberArray.shift();
+                }
+            }
+
+            else {
+                switch (buttonNumber) {
+                    case 1:
+                        button.textContent = "0";
+                        break;
+
+                    case 2:
+                        button.setAttribute("class", "button enter");
+                        button.textContent = "ENTER";
+                        break;
+
+                    case 3:
+                        button.textContent = "/";
+                        break;
+                }
+            }
+            if (rowNumber != 4 || buttonNumber != 4) {
+                row.appendChild(button);
+            }
+        }
+        buttons.appendChild(row);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", createButtons);
