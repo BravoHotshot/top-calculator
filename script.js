c9ebbb803a281bce75a1a8cbb5d41af3582fe97d
Bravo Hotshot <98163699+BravoHotshot@users.noreply.github.com>
2024-07-08 09:33:17 +0530

    Add calculator html, css layout and font

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ let add = (a, b) => a + b;
 let subtract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
 let divide = (a, b) => a / b;
+let operate = (a, op, b) => {
+    switch (op) {
+        case "+":
+            add(a, b);
+            break;
+
+        case "-":
+            subtract(a, b);
+            break;
+
+        case "*":
+            multiply(a, b);
+            break;
+
+        case "/":
+            divide(a, b);
+            break;
+    }
+}
